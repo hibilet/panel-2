8161ac3d93fc0f50ae25f339fd7c8882d31a834b
savrum <alp@savrum.com>
2026-03-01 18:37:27 +0100

    Refactor project structure: update entry point to index.jsx, remove unused App component and styles, and integrate Tailwind CSS for styling.

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles/index.css'
 
-createRoot(document.getElementById('root')).render(
+import App from './pages/app'
+
+createRoot(document.body).render(
   <StrictMode>
     <App />
   </StrictMode>,
